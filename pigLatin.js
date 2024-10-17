cac6a2c0356b279e90606318053939a77bdb54c8
Kayla Nadia <122777259+Classy-Cassy@users.noreply.github.com>
2024-10-17 22:00:39 +1300

    Create pigLatin.js

---
+++ pigLatin.js
@@ -0,0 +1,15 @@
+function pigLatin(sentence){
+    let vowels = ["a","e","i","o","u"]
+    let translation = "";
+    let consonant  = "";
+    let pigLatinWord = "";
+    for(let i = 0; i < sentence.length; i++){
+        if(vowels.includes(sentence[i])){
+            
+        }
+    }
+    
+    return translation;
+}
+
+console.log("   ".length);
